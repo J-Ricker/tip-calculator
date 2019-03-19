c4d1616022c036ed4cb0015b15186e76ab6ead40
Jeremy R <lvlgrinddesign@gmail.com>
2019-03-19 14:47:21 -0400

    setup services array

---
+++ main.js
@@ -0,0 +1,19 @@
+(function () {
+    // select options
+    const services = [
+        {
+            value: 1,
+            title: 'Great - 20%'
+        },
+        {
+            value: 2,
+            title: 'Ok - 10%'
+        },
+        {
+            value: 3,
+            title: 'Bad - 2%'
+        }
+    ];
+
+
+})();
