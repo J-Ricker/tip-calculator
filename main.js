9cf53ca6821228c67e8dfd336e1c00e946ca789c
Jeremy R <lvlgrinddesign@gmail.com>
2019-03-19 15:19:28 -0400

    loader and calculation functionality wokring

--- main.js
+++ main.js
@@ -22,5 +22,44 @@
 
         document.getElementById('input-service').appendChild(option);
     })
+    // get all the vlaues
+    const form = document.getElementById('tip-form');
+    const amount = document.getElementById('input-bill');
+    const users = document.getElementById('input-users');
+    const service = document.getElementById('input-service');
+    // customer feedback
+    const feedback = document.querySelector('.feedback');
+    const loader = document.querySelector('.loader');
+    const results = document.querySelector('.results');
+
+    form.addEventListener('submit', function (e) {
+        e.preventDefault();
+        let bill = amount.value;
+        let people = users.value;
+        let quality = services.value;
+
+        if (bill === '' || bill <= "0" || (people === '' || people <= "0") || quality === "0") {
+            feedback.classList.add('showItem', 'alert-danger');
+            feedback.innerHTML = `
+                <p>Please check the values</p>
+                <p>Bill amount cannot be less than 0</p>
+                <p>People sharing the bill cannot be less than 0</p>
+                <p>Service has to be selected</p>
+            `;
+            setTimeout(function () {
+                feedback.classList.remove('showItem', 'alert-danger');
+            }, 3000);
+        } else {
+            feedback.classList.add('showItem', 'alert-success');
+            feedback.innerHTML=    `
+                <p>Calculating...</p>
+            `;
+            loader.classList.add('showItem');
+            setTimeout(function () {
+                feedback.classList.remove('showItem');
+                loader.classList.remove('showItem');
+            }, 3000);
+        }
+    })
 
 })();
