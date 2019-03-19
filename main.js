c89f8598dade6276c75a1371dc5614d423b4824b
Jeremy R <lvlgrinddesign@gmail.com>
2019-03-19 14:56:51 -0400

    set up options in javascript

--- main.js
+++ main.js
@@ -14,6 +14,13 @@
             title: 'Bad - 2%'
         }
     ];
+    // add select options to the select element
+    services.forEach(function (service) {
+        const option = document.createElement('option');
+        option.textContent = service.title;
+        option.value = service.value;
 
+        document.getElementById('input-service').appendChild(option);
+    })
 
 })();
